f8c8d6644bfc18d679c1964e56eeb11a7edddd88
Alex <alexandru.dorultan@live.com>
2026-07-31 17:57:43 +0100

    dd

--- lerning.py
+++ lerning.py
@@ -1,6 +1,58 @@
+"""
 a = 45
 b = 33
 if b > a:
     print("b is greater than a")
 elif a == b:
     print("a is equal to b")
+"""
+"""
+score = 85
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")
+elif score >= 60:
+    print("Grade: D")
+else:
+    print("Not graded.")
+    """
+"""
+age = 40
+
+if age < 13:
+    print("You are a an child.")
+elif age < 20:
+    print("You are a teenager.")
+elif age < 65:
+    print("You are an adult.")
+elif age >= 65:
+    print("You are a senior citizen.")
+    """
+"""
+day = 75
+
+if day == 1:
+    print("Monday")
+elif day == 2:
+    print("Tuesday")
+elif day == 3:
+    print("Wednesday")
+elif day == 4:
+    print("Thursday")
+elif day == 5:
+    print("Friday")
+elif day == 6:
+    print("Saturday")
+elif day == 7:
+    print("Sunday")
+"""
+
+a = 200
+b = 23
+if b < a:
+    print("b is less than a")
+else:
+    print("b is not less than a")
