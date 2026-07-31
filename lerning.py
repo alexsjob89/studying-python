97e2952513b4eea7118bc60e0442a9d26ec735ee
Alex <alexandru.dorultan@live.com>
2026-07-31 18:30:14 +0100

    exrcise

--- lerning.py
+++ lerning.py
@@ -1,6 +1,27 @@
-a = 45
-b = 31
-if b > a:
-    print("b is greater than a")
-elif a == b:
-    print("a is equal to b")
+"""
+number = 7
+
+if number % 4 == 1:
+    print("The number is divisible by 4.")
+else:
+    print("The number is not divisible by 4.")
+"""
+"""
+temperature = 40
+
+if temperature >= 30:
+    print("It's a hot day.")
+elif temperature > 20:
+    print("It's a nice day.")
+elif temperature > 10:
+    print("It's a bit chilly.")
+else:
+    print("It's cold outside.")
+"""
+
+username = "Alex"
+
+if len(username) > 0:
+    print(f"Welcone, {username}!")
+else:
+    print(f"Error: Username {username} cannot be found!")
