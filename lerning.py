6cbecfae7bfcd518c4a9a5ce7a1453c6b1f2a553
alexsjob89 <alexandru.dorultan@live.com>
2026-08-19 00:25:53 +0100

    fn

--- lerning.py
+++ lerning.py
@@ -462,7 +462,7 @@ while True:
 """
 
 
-
+"""
 import random
 
 choices = ["rock", "paper", "scissors"]
@@ -509,7 +509,38 @@ while True:
 print("\n==== FINAL SCORE ===")
 print("Player:", player_score)
 print("Computer:", computer_score)
+"""
+"""
+def celsius_to_fahrenheit(celsius):
+    return(celsius * 9 / 5) + 32
 
+def fahrenheit_to_celsius(fahrenheit):
+    return(fahrenheit - 32) * 5 / 9
+
+
+print("Temperature Converter")
+print("1. Celsius -> Fahrenheit")
+print("2. Fahrenheit -> Celsius")
+
+choice = input("Chose an option: ")
+
+try:
+    temperature = float(input("Enter temperature: "))
+    
+    if choice == "1":
+        result = celsius_to_fahrenheit(temperature)
+        print(f"{temperature}C = {result:.2f}F")
+        
+    elif choice == "2":
+        result = fahrenheit_to_celsius(temperature)
+        print(f"{temperature}F = {result:.2f}C")
+        
+    else:
+        print("Invalid choice.")
+        
+except ValueError:
+    print("Please enter a valid number.")
+"""
 
 
 
