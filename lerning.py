1fbf85813ffe2601c8cc5fd0886d16d75f598e4d
alexsjob89 <alexandru.dorultan@live.com>
2026-08-23 10:13:22 +0100

    gt

--- lerning.py
+++ lerning.py
@@ -625,7 +625,7 @@ while True:
         print("Invalid option.")
 """
 
-
+"""
 import string
 import secrets
 
@@ -713,6 +713,57 @@ else:
     stremgth = "Strong"
     
 print("Password strength:", strength)
+"""
 
 
+# quiz game
 
+# questions
+
+questions = [
+    {
+        "question": "waht is the capital of France",
+        "options": ["A. London", "B. Paris", "C. Madrid", "D. Rome"],
+        "answer": "B"
+    },
+    {
+        "question": "Which language are we using?",
+        "options": ["A. Java", "B. C++", "C. Python", "D. PHP"],
+        "answer": "C"
+    },
+    {
+        "question": "What is 5 + 5?",
+        "options": ["A. 8", "B. 10", "C. 15", "D. 23"],
+        "answer": "B"
+    }
+]
+
+# score
+score = 0
+
+for question in questions:
+    
+    print("\n" + question["question"])
+    
+    for option in question["options"]:
+        print(option)
+        
+    answer = input("Your answer: ").upper()
+    
+    if answer == question["answer"]:
+        print("Correct!")
+        score += 1
+    
+    else:
+        print("Wrong!")
+        print("Correct anwer:", question["nswer"])
+        
+print("\nQuiz finished!")
+print(f"Your score: {score}/{len(questions)}")
+
+percentage = (score / len(questions)) * 100
+
+print(f"you scored {percentage:.0f}%")
+    
+    
+    
