2f476da14845d50c1a0abc971d0e577e3d7111fa
alexsjob89 <alexandru.dorultan@live.com>
2026-08-29 11:45:51 +0100

    rt

--- lerning.py
+++ lerning.py
@@ -1,5 +1,32 @@
-age = int(input("How are you? "))
 
-furture_age = age + 10
+"""
+number1 = float(input("Enter first number: "))
+number2 = float(input("Enter secound number: "))
 
-print(f"In 10 years you will be {furture_age}")
+print("Addition:", number1 + number2)
+print("Multiplication:", number1 * number2)
+print("Subtraction:", number1 - number2)
+print("Division:", number1 / number2)
+print("Procentage:", number1 % number2)
+"""
+"""
+names = ["Alex", "Elisa", "Kevin", "Even", "Ianos"]
+
+for name in names:
+    print(name)
+"""
+
+
+correct_password = "doru8989"
+
+password = input("Enter password: ")
+
+if correct_password == password:
+      print("Password is correct!")
+
+while password != correct_password:
+  
+      print("Incorrect passwod!")
+      password = input("TRy again: ")
+    
+      print("Login successful!")
